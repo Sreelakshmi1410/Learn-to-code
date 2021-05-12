4d8fca0a59150e191644e3c242a770cae1092b76
Sreelakshmi1410 <83947598+Sreelakshmi1410@users.noreply.github.com>
2021-05-12 19:49:09 +0530

    Create Day04.c

---
+++ Day04.c
@@ -0,0 +1,13 @@
+//Write a C Program to read an integer and find it's square root.
+
+
+#include <stdio.h>
+#include <math.h>
+ 
+int main()
+{
+    int a ;
+    scanf("%d", &a );
+    printf("%.1f\n",sqrt(a));
+    return 0;
+}

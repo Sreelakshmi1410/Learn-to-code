bfa9ca917286cbe540c2fb596560b952f20256bf
Sreelakshmi1410 <83947598+Sreelakshmi1410@users.noreply.github.com>
2021-05-12 19:04:34 +0530

    Update and rename README.md to Day01.c

---
+++ Day01.c
@@ -0,0 +1,13 @@
+// Write a C Program to print "Hello World!" and "Welcome to Learn To Code!" in two separate lines
+
+
+
+#include <stdio.h>
+
+int main() {
+   printf("Hello World!\n");
+   printf("Welcome to Learn To Code!");
+     
+ 
+   return 0;
+}

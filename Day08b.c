707208f3bd4a2ad8b8cb36ec63289daf13ee39c4
Sreelakshmi1410 <83947598+Sreelakshmi1410@users.noreply.github.com>
2021-05-19 20:47:21 +0530

    Update Day08b.c

--- Day08b.c
+++ Day08b.c
@@ -1,34 +1,27 @@
 //Write a C Program to compute the multiplication table of 1 - 5 using do-while.
 
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-int main()
-{
-int num, i=1;
-printf("\n1.Table of 1");
-printf("\n2.Table of 2");
-printf("\n3.Table of 3");
-printf("\n4.Table of 4");
-printf("\n5.Table of 5");
-printf("\n6.Exit\n");
-scanf("%d",&num);    
-if(num<=5)
-do
-{
-printf("%d ", num*i);
-i++;
-}while(i<=10);
-else
-printf("Enter a valid option!");
-printf("\n1.Table of 1");
-printf("\n2.Table of 2");
-printf("\n3.Table of 3");
-printf("\n4.Table of 4");
-printf("\n5.Table of 5");
-printf("\n6.Exit\n");
-return 0;
+#include<stdlib.h>
+int main() {
+   do
+   {
+       int i,n;
+       printf("\n1.Table of 1 \n2.Table of 2 \n3.Table of 3 \n4.Table of 4 \n5.Table of 5 \n6.Exit \n");
+       scanf("%d", &n);
+       if(n==6)
+       {
+           exit(0);
+       }
+       if(n>6)
+       {
+           printf("Enter a valid option!");
+       }
+       if(n<6)
+       {
+            for(i=1;i<=10;i++)
+            {
+              printf("%d ",n*i);
+            }
+       }
+   }while(1);
 }
-
